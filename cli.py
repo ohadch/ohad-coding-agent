e8320bf37e8cae19407a37ee340c0f368fabfced
ohad <ohad.c@orchid.security>
2024-12-08 10:58:05 +0200

    feat: supports gitignore

--- cli.py
+++ cli.py
@@ -1,12 +1,12 @@
+from dataclasses import dataclass
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from typing import Dict, List
 
-from pydantic import BaseModel
-
 from src import get_settings
-
 
 from src.services.coding_service import CodingService
 from src.services.repository_reader_service import RepositoryReaderService
@@ -25,14 +25,17 @@ def read_included_files(local_repo_path: str) -> Dict[str, str]:
     include_files = include_files.split(",") if include_files else None
 
     contents = RepositoryReaderService().read_files(
-        directory=local_repo_path, include_files=include_files
+        directory=local_repo_path,
+        include_files=include_files
     )
 
     print(f"Read {len(contents)} files from {local_repo_path}")
 
     return contents
 
-def run_code_writing_session(contents: Dict[str, str], local_repo_path: str):
+
+def run_code_writing_session(local_repo_path: str):
+    contents = read_included_files(local_repo_path=local_repo_path)
     task = input("Please give me a task: ")
     coding_service = CodingService()
     coding_service.learn_code(file_abs_path_to_content=contents)
@@ -45,45 +48,45 @@ def run_code_writing_session(contents: Dict[str, str], local_repo_path: str):
 
     print(f"Code written to {local_repo_path} successfully.")
 
-def run_bug_finder_session(contents: Dict[str, str], local_repo_path: str):
+
+def run_bug_finder_session(local_repo_path: str) -> object:
     raise NotImplementedError
 
 
-class MenuOption(BaseModel):
+@dataclass
+class MenuOption:
     option: str
     func: callable
-
-
 
 
 def main():
     settings = get_settings()
     local_repo_path = settings.repo_path
-    contents = read_included_files(local_repo_path=local_repo_path)
-
     menu_options: List[MenuOption] = [
-        MenuOption(option="Code Writing", func=lambda: run_code_writing_session(
-            contents=contents, local_repo_path=local_repo_path
-        )),
-        MenuOption(option="Bug Finder", func=lambda: run_bug_finder_session(
-            contents=contents, local_repo_path=local_repo_path
-        )),
+        MenuOption(
+            option="Code Writing",
+            func=lambda: run_code_writing_session(
+                local_repo_path=local_repo_path
+            )
+        ),
+        MenuOption(
+            option="Bug Finder",
+            func=lambda: run_bug_finder_session(
+                local_repo_path=local_repo_path
+            )
+        ),
     ]
 
     while True:
         print("Please choose an option:")
 
         for idx, option in enumerate(menu_options):
-            print(f"{idx + 1}. {option.option}")
+            print(f"{idx}. {option.option}")
 
         choice = input("Choose an option: ")
         choice = int(choice)
 
-        if choice not in range(1, len(menu_options) + 1):
-            print("Invalid choice. Please try again.")
-            continue
-
-        menu_options[choice].func(contents=contents, local_repo_path=local_repo_path)
+        menu_options[choice].func()
 
         continue_execution = input("Do you want to do something else? (yes/no) ")
 
--- src/services/repository_reader_service.py
+++ src/services/repository_reader_service.py
@@ -2,6 +2,9 @@ import logging
 import os
 from typing import Dict, List
 
+import pathspec
+from pathspec import PathSpec
+
 
 class RepositoryReaderService:
     EXCLUDED_FOLDERS = [
@@ -15,11 +18,28 @@ class RepositoryReaderService:
     def __init__(self):
         self._logger = logging.getLogger(__name__)
 
+    @staticmethod
+    def _read_gitignore_spec(gitignore_path: str) -> PathSpec:
+        with open(gitignore_path, "r") as file:
+            gitignore_patterns = file.read()
+
+        # Compile the patterns into a PathSpec object
+        spec = pathspec.PathSpec.from_lines('gitwildmatch', gitignore_patterns.splitlines())
+
+        return spec
+
+
     # a function that returns a dict of all files under a directory with their content.
     # the key is the absolute path of the file and the value is the content of the file.
     def read_files(
         self, directory: str, include_files: List[str] = None
     ) -> Dict[str, str]:
+        # Read .gitignore file if it exists
+        if os.path.exists(os.path.join(directory, ".gitignore")):
+            gitignore_spec = self._read_gitignore_spec(os.path.join(directory, ".gitignore"))
+        else:
+            gitignore_spec = None
+
         file_contents = {}
 
         # Walk through the directory tree
@@ -30,6 +50,9 @@ class RepositoryReaderService:
 
             for file_name in files:
                 if include_files and file_name not in include_files:
+                    continue
+
+                if gitignore_spec and gitignore_spec.match_file(os.path.relpath(os.path.join(root, file_name), start=directory)):
                     continue
 
                 file_path = os.path.join(root, file_name)
